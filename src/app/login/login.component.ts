df93940454b00565a24bca1c4e0c69ed1c12ff56
jonnasnovaes <jhonnata38@hotmail.com>
2021-10-13 19:53:48 -0300

    Add - Adicionado spinner de loading na tela de login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {FormControl, FormGroup} from '@angular/forms';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {AlertModalComponent} from "../core/alert-modal/alert-modal.component";
 
 @Component({
   selector: 'app-login',
@@ -11,30 +13,41 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form = new FormGroup({
-    email: new FormControl(''),
-    senha: new FormControl('')
+    email: new FormControl('coordenador@sams.gov.br'),
+    senha: new FormControl('123')
   });
+
+  loading = false;
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private modal: NgbModal
   ) { }
 
   ngOnInit(): void {
   }
 
   async logar(): Promise<any> {
+    this.loading = true;
+
     const perfilAtivo = await this.authService.login(this.form.value);
 
-    if (perfilAtivo === 0) {
-      await this.router.navigateByUrl('dashboard/cadastrar-vacina');
-    }
-    else if (perfilAtivo === 1 || perfilAtivo === 2) {
-      await this.router.navigateByUrl('dashboard/relatorio-dashboard');
-    }
-    else if (!perfilAtivo) {
-      console.log('Erro de login');
-    }
+    setTimeout(async () => {
+      if (perfilAtivo === 0) {
+        await this.router.navigateByUrl('dashboard/cadastrar-vacina');
+      }
+      else if (perfilAtivo === 1 || perfilAtivo === 2) {
+        await this.router.navigateByUrl('dashboard/relatorio-dashboard');
+      }
+      else if (!perfilAtivo) {
+        const alertModal = this.modal.open(AlertModalComponent, {size: 'md'});
+        alertModal.componentInstance.message = 'Erro ao tentar logar no sistema.';
+      }
+
+      this.loading = false;
+    }, 3000);
+
   }
 
 }
